024e72b2a103415c8cf00fb0f9025aab0e7d786e
chrisyao4700 <chrisyao900908@gmail.com>
2023-03-08 19:27:42 -0800

    Save for newest questions

--- question.ts
+++ question.ts
@@ -21,3 +21,55 @@ const main = (async () => {
     compareTwoTimesCheckTheSecondTimeIsEarlier('2020-01-01 00:00:01', '2020-01-01 00:00:00');
 
 })();
+
+
+/* Write a function that takes in an array of numbers and returns the largest number in the array.*/
+export const findLargestNumberInArray = (array: number[]): number => {
+    return 0;
+}
+
+/*Write a function that takes in an array of numbers and returns the sum of all of the odd numbers in the array.*/
+export const sumOfOddNumbersInArray = (array: number[]): number => {
+    return 0;
+}
+
+/*Given an array of strings, return the longest one in the array.*/
+export const longestStringInArray = (array: string[]): string => {
+    return '';
+}
+
+/* Given an input of string(Assume that the input string only contains letters, no numbers or special characters),write a function that returns the number of vowels in the string. */
+export const countVowelsInString = (string: string): number => {
+    return 0;
+}
+
+/* Write a function that reverse the input string. For example: “Animal” to “laminA”. */
+export const reverseString = (string: string): string => {
+    return '';
+}
+
+/* Given two integers, write a function that takes returns the smallest common divisor of these two numbers.*/
+export const smallestCommonDivisor = (number1: number, number2: number): number => {
+    return 0;
+}
+
+/*Given a word, return if this word is palindromes. For example, ‘level’, ‘civic’, ‘kayak’ are palindromes because they reads the same way forwards and backwards.*/
+export const isPalindrome = (string: string): boolean => {
+    return false;
+}
+
+/*Given an array of numbers, return a new array with all the even numbers doubled.*/
+export const doubleEvenNumbersInArray = (array: number[]): number[] => {
+    return [];
+}
+
+/*Given two arrays of numbers, write a function that returns a new array containing all the numbers that are common to both arrays in ascending order.*/
+export const commonNumbersInTwoArrays = (array1: number[], array2: number[]): number[] => {
+    return [];
+}
+
+/*Given a string, write a function that returns the most common letter in the string.*/
+export const mostCommonLetterInString = (string: string): string => {
+    return '';
+}
+
